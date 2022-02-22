4e20afd5227e18f9e15780e474afd14e70e3263d
Romario <morrisromo9966@gmail.com>
2022-02-21 23:42:24 -0500

    modularized app

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+
+//connect to the database
+const db = mysql.createConnection({
+    host: 'localhost',
+    //my MySQL username
+    user: 'root',
+    //my MySQL password
+    password: 'Invincible3162!',
+    database: 'business'
+},
+    console.log('Connected to the election database.')
+);
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+const db = require('./db/connection');
 const express = require('express');
-const res = require('express/lib/response');
-const mysql = require('mysql2');
+const apiRoutes = require('./routes/apiRoutes');
 const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
@@ -10,17 +10,17 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//connect to the database
-const db = mysql.createConnection({
-    host: 'localhost',
-    //my MySQL username
-    user: 'root',
-    //my MySQL password
-    password: 'Invincible3162!',
-    database: 'business'
-},
-    console.log('Connected to the election database.')
-);
+//Use api routes
+app.use('/api', apiRoutes);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 inquirer
     .prompt([
